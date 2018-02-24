88867ec6c3daf689a6565dc63b0224f33277966c
User <lehaazarenko@mail.ru>
2018-02-24 21:23:03 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let result = 0;
+  
+  function calc(num, x) {
+    let count = 0;
+    while (num > x) {
+      num -= x;
+      count++;
+    }
+    return count;
+  } 
+  
+  for (let i = 1; i < 12; i++) {
+  	let temp = Math.pow(5,i);
+    result += calc(number, temp);
+    if (number % temp === 0) {
+      result++;
+    }
+  }
+  
+  return result;
 }
+// 84583674
+// 21145911
